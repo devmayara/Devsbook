85c799d2bb29613108d498339ebb5c3bbda17064
Mayara Silva <devmayara11@gmail.com>
2022-08-01 08:27:58 -0300

    Detectando Login

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -1,20 +1,23 @@
 <?php
+
 namespace src\controllers;
 
 use \core\Controller;
+use \src\handlers\LoginHandler;
 
 class HomeController extends Controller {
 
+    private $loggerUser;
+
+    public function __construct() {
+        $this->loggerUser = LoginHandler::checkLogin();
+        if (LoginHandler::checkLogin() === false) {
+            $this->redirect('/login');
+        }
+    }
+
     public function index() {
         $this->render('home', ['nome' => 'Mayara']);
-    }
-
-    public function sobre() {
-        $this->render('sobre');
-    }
-
-    public function sobreP($args) {
-        print_r($args);
     }
 
 }
---
+++ src/controllers/LoginController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace src\controllers;
+
+use \core\Controller;
+
+class LoginController extends Controller {
+
+    public function signin() {
+        echo '<h1>Login</h1>';
+    }
+
+    public function signup() {
+        echo '<h1>Cadastro</h1>';
+    }
+
+}
