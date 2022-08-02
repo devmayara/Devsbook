9f9542a314ec3bbf98751de82b8fffc5faf20ea8
Mayara Silva <devmayara11@gmail.com>
2022-08-02 17:29:54 -0300

    Separando Feed e Feed Editor

---
+++ src/views/partials/feed-editor.php
@@ -0,0 +1,15 @@
+
+<div class="box feed-new">
+    <div class="box-body">
+        <div class="feed-new-editor m-10 row">
+            <div class="feed-new-avatar">
+                <img src="media/avatars/default.jpg" />
+            </div>
+            <div class="feed-new-input-placeholder">O que você está pensando, Mayara?</div>
+            <div class="feed-new-input" contenteditable="true"></div>
+            <div class="feed-new-send">
+                <img src="<?= $base; ?>/assets/images/send.png" />
+            </div>
+        </div>
+    </div>
+</div>
