dd6ce55494a0c2fd7c8b23d22014ffa1cad92ff6
Mayara Silva <devmayara11@gmail.com>
2022-08-02 17:51:25 -0300

    Feed Editor

--- src/views/pages/home.php
+++ src/views/pages/home.php
@@ -9,7 +9,7 @@
             <div class="row">
                 <div class="column pr-5">
 
-                    <?= $render('feed-editor'); ?>
+                    <?= $render('feed-editor', ['user'=>$loggedUser]); ?>
                     
                     <?= $render('feed-item'); ?>
 
--- src/views/partials/feed-editor.php
+++ src/views/partials/feed-editor.php
@@ -3,13 +3,31 @@
     <div class="box-body">
         <div class="feed-new-editor m-10 row">
             <div class="feed-new-avatar">
-                <img src="media/avatars/default.jpg" />
+                <img src="<?= $base; ?>/media/avatars/<?= $user->avatar; ?>" />
             </div>
-            <div class="feed-new-input-placeholder">O que você está pensando, Mayara?</div>
+            <div class="feed-new-input-placeholder">O que você está pensando, <?= $user->name; ?>?</div>
             <div class="feed-new-input" contenteditable="true"></div>
             <div class="feed-new-send">
                 <img src="<?= $base; ?>/assets/images/send.png" />
             </div>
+            <form class="feed-new-form" action="<?= $base; ?>/post/new" method="POST">
+                <input type="hidden" name="body" />
+            </form>
         </div>
     </div>
 </div>
+
+<script type="text/javascript">
+    let feedInput = document.querySelector('.feed-new-input');
+    let feedSbumit = document.querySelector('.feed-new-send');
+    let feedForm = document.querySelector('.feed-new-form');
+
+    feedSbumit.addEventListener('click', function(obj) {
+        let value = feedInput.innerHTML;
+
+        if (value != '') {
+            feedForm.querySelector('input[name="body"]').value = value;
+            feedForm.submit();
+        }
+    });
+</script>
